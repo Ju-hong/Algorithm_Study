01149eac5558973a697631da082c936df364e7a0
Ju-hong <luyenen@gmail.com>
2025-01-15 11:35:49 +0900

    [level 0] Title: 배열 만들기 4, Time: 35.39 ms, Memory: 13.8 MB -BaekjoonHub

--- 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.py
+++ 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.py
@@ -2,10 +2,9 @@ def solution(arr):
     stk = [0]
     i = 0
     while i < len(arr):
-        if stk[-1] >= arr[i]:
+        if stk[-1] >= arr[i]: # stk에 0을 넣어주지 않으면 out of range error
             stk.pop()
         else:
             stk.append(arr[i])
             i += 1
     return stk[1:]
-        
