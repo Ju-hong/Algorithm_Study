41267eb42f29fab16042d24a1218f6e22bfc42c7
Ju-hong <luyenen@gmail.com>
2025-02-25 08:08:25 +0900

    [Bronze III] Title: 그대로 출력하기, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.py
@@ -0,0 +1,14 @@
+import sys
+
+lines = sys.stdin.readlines()
+
+for line in lines:
+    print(line.strip())
+    
+'''
+while True:
+    try:
+        print()
+    else EOFError:
+        break
+'''
