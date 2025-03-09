ed2ae62ccabf8167ea33d14116ee551473620ae3
Ju-hong <luyenen@gmail.com>
2025-03-09 12:05:14 +0900

    [Bronze III] Title: ACM 호텔, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/10250. ACM 호텔/ACM 호텔.py
+++ 백준/Bronze/10250. ACM 호텔/ACM 호텔.py
@@ -3,19 +3,13 @@ k = int(sys.stdin.readline().strip())
 
 for _ in range(k):
     h, _, n = map(int, sys.stdin.readline().strip().split())
-    if h == 1:
-        i = str(1)
-        j = str(n)
 
-    
-    elif not n%h: #나누어 떨어짐
-        i = str(h)
-        j = str(n//h)
+    if not n%h: 
+        i = h 
+        j = n//h
  
     else:
-        i = str(n%h)
-        j = str(n//h+1)
-
-    if len(j) == 1:
-        j = str(0)+j
-    print(i+j)
+        i = n%h
+        j = n//h+1
+        
+    print(i*100+j)
