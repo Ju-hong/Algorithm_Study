f337f750d093424f75a4699d295835109b102bf0
Ju-hong <luyenen@gmail.com>
2025-03-10 10:52:17 +0900

    [Silver V] Title: 아파트, Time: 40 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Silver/32979. 아파트/아파트.py
+++ 백준/Silver/32979. 아파트/아파트.py
@@ -1,11 +1,14 @@
-import sys
-from collections import deque
+import sys 
 
 _ = int(sys.stdin.readline().strip())
 n = int(sys.stdin.readline().strip())
-queing = deque(map(int, sys.stdin.readline().strip().split()))
+queing = list(map(int, sys.stdin.readline().strip().split()))
 count =  list(map(int, sys.stdin.readline().strip().split()))
 
+num = 0
+
 for i in range(n):
-    queing.rotate(-(count[i])+1)
-    print(queing[0], end=" ")
+    num = num + count[i] - 1
+    if num >= len(queing):
+        num = num % len(queing)
+    print(queing[num], end=" ")
