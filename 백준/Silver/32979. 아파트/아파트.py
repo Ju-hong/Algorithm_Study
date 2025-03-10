98c7f7d3d1ed5d737f46d641cf689cdb2d457a79
Ju-hong <luyenen@gmail.com>
2025-03-10 10:53:20 +0900

    [Silver V] Title: 아파트, Time: 40 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Silver/32979. 아파트/아파트.py
+++ 백준/Silver/32979. 아파트/아파트.py
@@ -8,7 +8,5 @@ count =  list(map(int, sys.stdin.readline().strip().split()))
 num = 0
 
 for i in range(n):
-    num = num + count[i] - 1
-    if num >= len(queing):
-        num = num % len(queing)
+    num = (num + count[i] - 1) % len(queing)
     print(queing[num], end=" ")
