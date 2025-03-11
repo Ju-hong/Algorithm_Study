705d25613e6c08663702623debc5ddd59d1416fd
Ju-hong <luyenen@gmail.com>
2025-03-11 11:26:53 +0900

    [Bronze I] Title: 부녀회장이 될테야, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.py
+++ 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.py
@@ -7,15 +7,10 @@ temp = [int(num.strip()) for num in sys.stdin.readlines()]
 for num1 in range(n):
     floor = temp[2*num1]     #1층
     number = temp[2*num1+1] #3호
+    zero = [_ for _ in range(1, number+1)] 
 
-
-    zero = [k for k in range(1, number+1)] #0번 위치
-
-    for num2 in range(floor): # 1
-        new = []
-
-        for num3 in range(number): # 3
-            new.append(sum(zero[:num3+1]))
-
-        zero = new
+    for num2 in range(floor): 
+        for num3 in range(1, number):
+            zero[num3] += zero[num3-1]
+    
     print(zero[-1])
