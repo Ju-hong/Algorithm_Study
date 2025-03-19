3d13b5d0af02d593ff7753750f9671cd02855413
Ju-hong <luyenen@gmail.com>
2025-03-19 11:25:56 +0900

    [Silver II] Title: 가장 긴 감소하는 부분 수열, Time: 56 ms, Memory: 36976 KB -BaekjoonHub

---
+++ 백준/Silver/11722. 가장 긴 감소하는 부분 수열/가장 긴 감소하는 부분 수열.py
@@ -0,0 +1,22 @@
+from bisect import bisect_left, bisect_right
+import sys
+from collections import deque
+
+
+n = int(sys.stdin.readline().rstrip())
+
+arr = list(map(int, sys.stdin.readline().rstrip().split()))
+dp = deque([arr[0]]) # 긴 수열 저장
+len_arr = [1] # 길이 저장
+
+
+for i in range(1, n):
+    if arr[i] < dp[0]: # arr[i]가 현재 최소보다 작다
+        dp.appendleft(arr[i]) # 내림차순
+        len_arr.append(len_arr[-1]+1)
+        
+    else: # arr[i]가 현재 최소보다 작다
+        idx = bisect_right(dp, arr[i])
+        dp[idx-1] = arr[i]
+
+print(len_arr[-1])
