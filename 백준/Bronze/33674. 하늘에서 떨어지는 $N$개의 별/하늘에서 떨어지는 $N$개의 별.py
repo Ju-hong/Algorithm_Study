bb000ed4af02a245ff2088666e2a19d86a4b8293
Ju-hong <luyenen@gmail.com>
2025-03-31 22:18:25 +0900

    [Bronze II] Title: 하늘에서 떨어지는 $N$개의 별, Time: 36 ms, Memory: 34536 KB -BaekjoonHub

--- 백준/Bronze/33674. 하늘에서 떨어지는 $N$개의 별/하늘에서 떨어지는 $N$개의 별.py
+++ 백준/Bronze/33674. 하늘에서 떨어지는 $N$개의 별/하늘에서 떨어지는 $N$개의 별.py
@@ -1,8 +1,8 @@
 import math
 
-n, d, k = map(int, input().split())
-arr = list(map(int, input().split()))
+_, d, k = map(int, input().split())
+maxi = max(map(int, input().split()))
 
-doom_cycle = k//max(arr)
+doom_cycle = k//maxi
 
 print(math.ceil((d-doom_cycle)/doom_cycle))
