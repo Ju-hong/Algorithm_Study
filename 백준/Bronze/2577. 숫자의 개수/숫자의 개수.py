3858374038169a669427ed509bc5165d2cc85e16
Ju-hong <luyenen@gmail.com>
2025-04-04 22:25:39 +0900

    [Bronze II] Title: 숫자의 개수, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2577. 숫자의 개수/숫자의 개수.py
@@ -0,0 +1,10 @@
+import sys
+
+a, b, c = map(int, sys.stdin.readlines())
+dict_i = {'0':0, '1':0, '2':0, '3':0, '4':0, '5':0, '6':0, '7':0, '8':0, '9':0}
+
+for i in str(a*b*c):
+    dict_i[i] += 1
+
+for j in range(len(dict_i)):
+    print(dict_i[str(j)])
