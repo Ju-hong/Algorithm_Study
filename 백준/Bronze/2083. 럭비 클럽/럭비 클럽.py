09a48781cb33e30469a5f34919d0293087d26b0d
Ju-hong <luyenen@gmail.com>
2025-04-19 14:28:14 +0900

    [Bronze IV] Title: 럭비 클럽, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2083. 럭비 클럽/럭비 클럽.py
+++ 백준/Bronze/2083. 럭비 클럽/럭비 클럽.py
@@ -4,8 +4,4 @@ while True:
     n, m, k = sys.stdin.readline().split()
     if n == '#':
         break
-    if int(m) > 17 or int(k) >=80:
-        print(n, 'Senior')
-    else:
-        print(n, 'Junior')
-        
+    print(f'{n} Senior' if int(m) > 17 or int(k) >=80 else f"{n} Junior")
