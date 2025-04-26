f5995136a2c4c2aeb83d04a6be9a1f26f3a2e763
Ju-hong <luyenen@gmail.com>
2025-04-26 14:53:16 +0900

    [Silver IV] Title: 동전 0, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.py
+++ 백준/Silver/11047. 동전 0/동전 0.py
@@ -1,12 +1,12 @@
 import sys
 
 n, m = map(int, sys.stdin.readline().split())
-arr = list(map(int, list(sys.stdin.readline() for _ in range(n))))
-sorted_arr = sorted(arr, reverse = True)
+arr = [int(sys.stdin.readline()) for _ in range(n) ]
+arr.reverse()
 
 result = []
 
-for i in sorted_arr:
+for i in arr:
     temp = m//i
     result.append(temp)
     m -= temp*i
