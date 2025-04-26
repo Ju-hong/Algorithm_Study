88aadfe187f06d5f240352f1ed21cf2b9b019cbd
Ju-hong <luyenen@gmail.com>
2025-04-26 14:55:52 +0900

    [Silver IV] Title: 동전 0, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.py
+++ 백준/Silver/11047. 동전 0/동전 0.py
@@ -4,11 +4,10 @@ n, m = map(int, sys.stdin.readline().split())
 arr = [int(sys.stdin.readline()) for _ in range(n) ]
 arr.reverse()
 
-result = []
+result = 0
 
 for i in arr:
-    temp = m//i
-    result.append(temp)
-    m -= temp*i
+    result += m//i
+    m = m % i
 
-print(sum(result))
+print(result)
