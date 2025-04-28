0afa6cce140153bc5f4f7495facf84cec7f8141c
Ju-hong <luyenen@gmail.com>
2025-04-28 12:45:20 +0900

    [Bronze III] Title: 웰컴 키트, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/30802. 웰컴 키트/웰컴 키트.py
+++ 백준/Bronze/30802. 웰컴 키트/웰컴 키트.py
@@ -1,14 +1,13 @@
 import sys 
 
-_ = int(sys.stdin.readline())
+k = int(sys.stdin.readline())
 arr = list(map(int, sys.stdin.readline().split()))
 n, m = map(int, sys.stdin.readline().split())
 
 shirt_count = 0
 
 for i in arr:
-    if 1 <= i:
-        shirt_count = shirt_count + (i-1)//n + 1
+    shirt_count += (i-1)//n + 1
 
 print(shirt_count)
-print(sum(arr)//m, sum(arr)%m)
+print(k//m, k%m)
