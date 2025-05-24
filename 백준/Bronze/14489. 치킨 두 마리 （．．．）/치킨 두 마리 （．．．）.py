b89616d4d8443ed8df33b7a0d547152d3fdf009a
Juhong Lee <luyenen@gmail.com>
2025-05-24 17:06:57 +0900

    [Bronze IV] Title: 치킨 두 마리 (...), Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/14489. 치킨 두 마리 （．．．）/치킨 두 마리 （．．．）.py
+++ 백준/Bronze/14489. 치킨 두 마리 （．．．）/치킨 두 마리 （．．．）.py
@@ -1,7 +1,4 @@
 sum_i = sum(map(int, input().split()))
 chick_two = int(input()) * 2
 
-if sum_i >= chick_two :
-    print(sum_i-chick_two)
-else:
-    print(sum_i)
+print(sum_i-chick_two if sum_i >= chick_two else sum_i)
